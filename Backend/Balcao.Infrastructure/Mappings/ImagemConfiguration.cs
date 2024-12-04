d146bc0a40b9ed4a0cf193c895654944da10767a
Roger Castellar <rogerpcto@yahoo.com.br>
2024-12-04 01:00:15 -0300

    Adiciona mapeamento de entidades
    - Cria também primeira migração

---
+++ Backend/Balcao.Infrastructure/Mappings/ImagemConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Balcao.Domain.Entities;
+
+namespace Balcao.Infrastructure.Mappings
+{
+    public class ImagemConfiguration : IEntityTypeConfiguration<Imagem>
+    {
+        public void Configure(EntityTypeBuilder<Imagem> builder)
+        {
+            #region Key
+
+            builder.HasKey(i => i.Id);
+
+            #endregion
+
+            #region Properties
+
+            builder.Property(i => i.Url)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            #endregion
+
+            #region Relationships
+
+            builder.HasOne<Anuncio>()
+                .WithMany(a => a.Imagem)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            #endregion
+        }
+    }
+}
+
---
+++ Backend/Balcao.Infrastructure/Mappings/UsuarioConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Balcao.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Balcao.Infrastructure.Mappings
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            #region Key
+            
+            builder.HasKey(u => u.Id);
+
+            #endregion
+
+            #region Properties
+            
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(u => u.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Senha)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Nota)
+                .IsRequired();
+
+            builder.Property(u => u.Perfil)
+                .IsRequired()
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Perfil)Enum.Parse(typeof(Perfil), v)
+                );
+
+            #endregion
+
+            #region Relationships
+            
+            builder.HasMany(u => u.Compras)
+                .WithOne(c => c.Comprador)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            #endregion
+        }
+    }
+
+}
