efbe9cad891c0d65c04a765da428dbaa7a007c61
JoaoVMansur <jvictormansur@gmail.com>
2024-12-06 19:08:30 -0300

    Cria rota para login e hasheia a senha ao  criar usuario
    remove login dto

--- Backend/Balcao.Domain/Entities/Usuario.cs
+++ Backend/Balcao.Domain/Entities/Usuario.cs
@@ -1,23 +1,36 @@
-﻿namespace Balcao.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Balcao.Domain.Entities
 {
     public class Usuario
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string Nome { get; set; }
+        [JsonIgnore]
         public string Senha { get; set; }
         public float Nota { get; set; }
         public Perfil Perfil { get; set; }
         public List<Compra> Compras { get; set; } = new List<Compra>();
 
-        public void Logar()
+        public bool Logar(string requisicaoSenha, string senha)
         {
-            throw new NotImplementedException();
-        }
+            if (string.IsNullOrEmpty(senha))
+            {
+                return false;
+            }
 
+            return VerificarSenha(requisicaoSenha, senha);
+        }
+        private bool VerificarSenha(string requisicaoSenha, string senha)
+        {
+            return BCrypt.Net.BCrypt.Verify(requisicaoSenha, senha);
+        }
         public void Logout()
         {
             throw new NotImplementedException();
         }
     }
+
+
 }
