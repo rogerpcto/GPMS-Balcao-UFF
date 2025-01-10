0b93a90993cb4d2b0d5fddb4332ac57834b403a1
Logan Miranda <loganmiranda@id.uff.br>
2025-01-10 12:20:40 -0300

    Migração de Ajuste de Mensagem
    - Adiciona um bool para saber de quem é a mensagem

---
+++ Backend/Balcao.Infrastructure/Migrations/20250110032519_AjusteMensagem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Balcao.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteMensagem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Proprietario",
+                table: "Mensagem",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Proprietario",
+                table: "Mensagem");
+        }
+    }
+}
